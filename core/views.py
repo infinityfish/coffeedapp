063c8e30d32195d39ef09249f7c65e4069d232a3
ericmabo <infinityfish360@gmail.com>
2015-05-20 10:56:31 -0400

    First commit! Added Template and static files

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# from django.http import HttpResponse
+# Create your views here.
+
+# def TestView(request, **kwargs):
+# 	return HttpResponse("Hello 237SPACES")
+
+class SplashView(TemplateView):
+	template_name = "index.html"
+
